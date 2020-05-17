a1c6b58aee615ae1c1298eee3782476cb34d2f05
Joshua Laquai <J.laquai@gmx.de>
2020-05-17 02:34:18 +0200

    rarities added

--- solution_alpha-pack-roll-generator/MainWindow.xaml.cs
+++ solution_alpha-pack-roll-generator/MainWindow.xaml.cs
@@ -1,28 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace solution_alpha_pack_roll_generator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            LB_Rarities.ItemsSource = LoadCollectionData();
+            // resize columns
         }
+
+        private List<AlphaPack> LoadCollectionData()
+        {
+            List<AlphaPack> packs = new List<AlphaPack>
+            {
+                new AlphaPack()
+                {
+                    Rarity = "Common",
+                    Chance = 0.33
+                },
+
+                new AlphaPack()
+                {
+                    Rarity = "Uncommon",
+                    Chance = 0.285
+                },
+
+                new AlphaPack()
+                {
+                    Rarity = "Rare",
+                    Chance = 0.224
+                },
+
+                new AlphaPack()
+                {
+                    Rarity = "Epic",
+                    Chance = 0.125
+                },
+
+                new AlphaPack()
+                {
+                    Rarity = "Legendary",
+                    Chance = 0.036
+                }
+            };
+
+            return packs;
+        }
+    }
+
+    public class AlphaPack
+    {
+        public string Rarity { get; set; }
+        public double Chance { get; set; }
     }
 }
