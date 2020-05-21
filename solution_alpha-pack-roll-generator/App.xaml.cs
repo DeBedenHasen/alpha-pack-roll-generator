7460647b686530d0e26ccc4b16d193069e62de84
Joshua Laquai <J.laquai@gmx.de>
2020-05-21 23:26:14 +0200

    5/21/20

--- solution_alpha-pack-roll-generator/App.xaml.cs
+++ solution_alpha-pack-roll-generator/App.xaml.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace solution_alpha_pack_roll_generator
+﻿namespace solution_alpha_pack_roll_generator
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- solution_alpha-pack-roll-generator/MainWindow.xaml.cs
+++ solution_alpha-pack-roll-generator/MainWindow.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
-
-namespace solution_alpha_pack_roll_generator
+﻿namespace solution_alpha_pack_roll_generator
 {
     public partial class MainWindow : Window
     {
@@ -11,52 +6,44 @@ namespace solution_alpha_pack_roll_generator
         {
             InitializeComponent();
 
-            LB_Rarities.ItemsSource = LoadCollectionData();
-            // resize columns
-        }
+            MessageBoxResult r = YesNo("This is all based on a Reddit post from July 19, 2017\nAt least *right now*, I really can't be arsed to count and find this data by myself. Maybe in the future, but... not now.\n\nWould you like to open the Reddit post?", "Reddit post");
 
-        private List<AlphaPack> LoadCollectionData()
-        {
-            List<AlphaPack> packs = new List<AlphaPack>
+            if (r == MessageBoxResult.Yes)
             {
-                new AlphaPack()
+                string browser = string.Empty;
+                RegistryKey key = null;
+                try
                 {
-                    Rarity = "Common",
-                    Chance = 0.33
-                },
-
-                new AlphaPack()
-                {
-                    Rarity = "Uncommon",
-                    Chance = 0.285
-                },
-
-                new AlphaPack()
-                {
-                    Rarity = "Rare",
-                    Chance = 0.224
-                },
-
-                new AlphaPack()
-                {
-                    Rarity = "Epic",
-                    Chance = 0.125
-                },
-
-                new AlphaPack()
-                {
-                    Rarity = "Legendary",
-                    Chance = 0.036
+                    key = Registry.ClassesRoot.OpenSubKey(@"HTTP\shell\open\command");
+                    if (key != null)
+                    {
+                        browser = key.GetValue(null).ToString().ToLower().Trim(new[] { '"' });
+                    }
+                    if (!browser.EndsWith("exe"))
+                    {
+                        browser = browser.Substring(0, browser.LastIndexOf(".exe", StringComparison.InvariantCultureIgnoreCase) + 4);
+                    }
                 }
-            };
+                finally
+                {
+                    if (key != null)
+                    {
+                        key.Close();
+                    }
+                }
 
-            return packs;
+                Process.Start(browser, "https://www.reddit.com/r/Rainbow6/comments/6o7o23/alpha_packs_statistics_and_info_1000_packs/");
+            }
         }
-    }
 
-    public class AlphaPack
-    {
-        public string Rarity { get; set; }
-        public double Chance { get; set; }
+        private void Rarities_Defaults_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private MessageBoxResult YesNo(string msg, string title)
+        {
+            return MessageBox.Show(msg, title, MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+        }
     }
 }
