42d7e95f9ef6f90bab1ee18e1b29e62e22567e55
Joshua Laquai <J.laquai@gmx.de>
2020-05-22 16:05:23 +0200

    5/22/20 04:03 PM - pretty much done now, need to add delay between RollControls_RollOnce_Click() calls in RollControls_RollUntil_Click()

--- solution_alpha-pack-roll-generator/MainWindow.xaml.cs
+++ solution_alpha-pack-roll-generator/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Diagnostics;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -8,22 +9,16 @@ namespace solution_alpha_pack_roll_generator
 {
     public partial class MainWindow : Window
     {
-        /*
-         * To-do
-         * =====
-         * - add counters for the rolled packs → RollUntil button
-         * - make RollUntil button... work
-         * - create different routines depnding on useCustom variable !!
-         */
-
         static readonly double[] rarities_valuesdefault = { 0.33, 0.285, 0.224, 0.125, 0.036 };
         static double[] rarities_valuescustom = { 0, 0, 0, 0, 0 };
 
         static bool useCustom = false;
 
-        static ComboBoxItem stopOnSelection;
-
         static TextBox[] rarities_boxeschances;
+
+        static TextBlock[] amounts;
+        static Button[] amounts_calls;
+        static int[] amounts_counters = { 0, 0, 0, 0, 0 };
 
         public MainWindow()
         {
@@ -36,6 +31,24 @@ namespace solution_alpha_pack_roll_generator
                 Rarities_3_Chance,
                 Rarities_4_Chance,
                 Rarities_5_Chance
+            };
+
+            amounts_calls = new Button[]
+            {
+                Rolls_Results_Amount_1_Label,
+                Rolls_Results_Amount_2_Label,
+                Rolls_Results_Amount_3_Label,
+                Rolls_Results_Amount_4_Label,
+                Rolls_Results_Amount_5_Label
+            };
+
+            amounts = new TextBlock[]
+            {
+                Rolls_Results_Amount_1,
+                Rolls_Results_Amount_2,
+                Rolls_Results_Amount_3,
+                Rolls_Results_Amount_4,
+                Rolls_Results_Amount_5
             };
 
             MessageBoxResult r = YesNo("This is all based on a Reddit post from July 19, 2017\nAt least *right now*, I really can't be arsed to count and find this data by myself. Maybe in the future, but... not now.\n\nWould you like to open the Reddit post?", "Reddit post");
@@ -87,9 +100,27 @@ namespace solution_alpha_pack_roll_generator
                 rarities_valuescustom[i] = Convert.ToDouble(rarities_boxeschances[i].Text);
             }
 
-            // Check if all of the custom values are equal to the defaults
-            // If yes, reset rarities_valuescustom[] to all 0s and change useCustom flag
-            // If not, do nothing
+            //Check if the values do not add up to 1
+            //If yes, show error, reset rarities_valuescustom[] to all 0s and change useCustom flag back
+            //If not, proceed
+            if (Sum(5, rarities_valuescustom) != 1)
+            {
+                for (int i = 0; i < rarities_valuescustom.Length; i++)
+                {
+                    rarities_valuescustom[i] = 0;
+                    rarities_boxeschances[i].Text = rarities_valuesdefault[i].ToString();
+                }
+
+                useCustom = false;
+
+                Error("The values entered do not add up to 1 and therefore cannot produce valid output!\nResetting to defaults.","Error");
+
+                return;
+            }
+
+            //Check if all of the custom values are equal to the defaults
+            //If yes, reset rarities_valuescustom[] to all 0s and change useCustom flag back
+            //If not, do nothing
             bool[] vs = { false, false, false, false, false };
 
             for (int i = 0; i < rarities_valuescustom.Length; i++)
@@ -111,48 +142,178 @@ namespace solution_alpha_pack_roll_generator
             }
         }
 
-        private void RollControls_RollOnce_Click(object sender, RoutedEventArgs e)
+        private double Sum(int howManyElements, double[] fromThisArray)
         {
-            Random ran = new Random();
-            double rollChance = Math.Round(ran.NextDouble(), 3);
+            double sum = 0.0;
 
-            if (rollChance > rarities_valuesdefault[0])
+            for (int i = 0; i < howManyElements; i++)
             {
-                Rolls_RollOnce_Result.Text = "Common";
+                sum += fromThisArray[i];
             }
-            else if (rollChance <= rarities_valuesdefault[0] && rollChance > rarities_valuesdefault[1])
-            {
-                Rolls_RollOnce_Result.Text = "Uncommon";
-            }
-            else if (rollChance <= rarities_valuesdefault[1] && rollChance > rarities_valuesdefault[2])
-            {
-                Rolls_RollOnce_Result.Text = "Rare";
-            }
-            else if (rollChance <= rarities_valuesdefault[2] && rollChance > rarities_valuesdefault[3])
-            {
-                Rolls_RollOnce_Result.Text = "Epic";
-            }
-            else if (rollChance < rarities_valuesdefault[3])
-            {
-                Rolls_RollOnce_Result.Text = "Legendary";
-            }
+
+            return sum;
         }
 
+        private void RollControls_RollOnce_Click(object sender, RoutedEventArgs e)
+        {
+            RollControls_RollOnce_Click();
+        }
 
+        private void RollControls_RollOnce_Click()
+        {
+            //Generate new random double
+            Random ran = new Random();
+            double rollChance = Math.Round(ran.NextDouble(), 4);
+
+            //Decide which set of values to use, then perform the checks on rollChance to determine the roll rarity,
+            //then update the connected counter
+            if (useCustom == true)
+            {
+                if (rollChance <= Sum(1, rarities_valuescustom))
+                {
+                    Rolls_RollOnce_Result.Text = "Common";
+                    amounts[0].Text = Convert.ToString(Convert.ToInt32(amounts[0].Text) + 1);
+                }
+                else if (rollChance <= Sum(2, rarities_valuescustom))
+                {
+                    Rolls_RollOnce_Result.Text = "Uncommon";
+                    amounts[1].Text = Convert.ToString(Convert.ToInt32(amounts[1].Text) + 1);
+                }
+                else if (rollChance <= Sum(3, rarities_valuescustom))
+                {
+                    Rolls_RollOnce_Result.Text = "Rare";
+                    amounts[2].Text = Convert.ToString(Convert.ToInt32(amounts[2].Text) + 1);
+                }
+                else if (rollChance <= Sum(4, rarities_valuescustom))
+                {
+                    Rolls_RollOnce_Result.Text = "Epic";
+                    amounts[3].Text = Convert.ToString(Convert.ToInt32(amounts[3].Text) + 1);
+                }
+                else if (rollChance > Sum(4, rarities_valuescustom))
+                {
+                    Rolls_RollOnce_Result.Text = "Legendary";
+                    amounts[4].Text = Convert.ToString(Convert.ToInt32(amounts[4].Text) + 1);
+                }
+                else
+                {
+                    Error("Out-of-range value for rollChance encountered: < " + rollChance + " >", "Error!");
+                }
+            }
+            else
+            {
+                if (rollChance <= Sum(1, rarities_valuesdefault))
+                {
+                    Rolls_RollOnce_Result.Text = "Common";
+                    amounts[0].Text = Convert.ToString(Convert.ToInt32(amounts[0].Text) + 1);
+                }
+                else if (rollChance <= Sum(2, rarities_valuesdefault))
+                {
+                    Rolls_RollOnce_Result.Text = "Uncommon";
+                    amounts[1].Text = Convert.ToString(Convert.ToInt32(amounts[1].Text) + 1);
+                }
+                else if (rollChance <= Sum(3, rarities_valuesdefault))
+                {
+                    Rolls_RollOnce_Result.Text = "Rare";
+                    amounts[2].Text = Convert.ToString(Convert.ToInt32(amounts[2].Text) + 1);
+                }
+                else if (rollChance <= Sum(4, rarities_valuesdefault))
+                {
+                    Rolls_RollOnce_Result.Text = "Epic";
+                    amounts[3].Text = Convert.ToString(Convert.ToInt32(amounts[3].Text) + 1);
+                }
+                else if (rollChance > Sum(4, rarities_valuesdefault))
+                {
+                    Rolls_RollOnce_Result.Text = "Legendary";
+                    amounts[4].Text = Convert.ToString(Convert.ToInt32(amounts[4].Text) + 1);
+                }
+                else
+                {
+                    Error("Out-of-range value for rollChance encountered: < " + rollChance + " >", "Error!");
+                }
+            }
+
+            //Update last rollChance display
+            Rolls_LastRollChance.Text = rollChance.ToString();
+
+            //Get new total and update the display
+            int newTotal = 0;
+
+            for (int i = 0; i < amounts.Length; i++)
+            {
+                newTotal += Convert.ToInt32(amounts[i].Text);
+            }
+
+            Rolls_TotalRolls.Text = newTotal.ToString();
+        }
 
         private void RollControls_RollUntil_Click(object sender, RoutedEventArgs e)
         {
+            RollControls_ResetAmounts_Click();
 
+            TextBlock target = null;
+
+            for (int i = 0; i < amounts_calls.Length; i++)
+            {
+                if (sender == amounts_calls[i])
+                {
+                    target = amounts[i];
+                }
+            }
+
+            if (target == null)
+            {
+                Error("Determining < target > returned < null >! It is likely the comparison to < sender > failed!\n< sender > == " + sender.ToString(), "Error");
+                return;
+            }
+
+            while (true)
+            {
+                RollControls_RollOnce_Click();
+
+                if (Convert.ToInt32(target.Text) != 0)
+                {
+                    break;
+                }
+
+                //Sleep(100);
+            }
         }
 
-        private void RollControls_StopOn_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Sleep(int ms)
         {
-            stopOnSelection = (ComboBoxItem)RollControls_StopOn.SelectedItem;
+            
         }
 
         private MessageBoxResult YesNo(string msg, string title)
         {
             return MessageBox.Show(msg, title, MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+        }
+        
+        private void Error(string msg, string title)
+        {
+            MessageBox.Show(msg, title, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void OK(string msg, string title)
+        {
+            MessageBox.Show(msg, title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void RollControls_ResetAmounts_Click(object sender, RoutedEventArgs e)
+        {
+            RollControls_ResetAmounts_Click();
+        }
+
+        private void RollControls_ResetAmounts_Click()
+        {
+            for (int i = 0; i < amounts_counters.Length; i++)
+            {
+                Rolls_RollOnce_Result.Text = "—";
+
+                amounts_counters[i] = 0;
+                amounts[i].Text = amounts_counters[i].ToString();
+                Rolls_TotalRolls.Text = 0.ToString();
+            }
         }
     }
 }
